74b1802968a2265499a2c619a25775a6247265f2
Naresh Podichetty <nareshpodi@hotmail.com>
2017-07-20 20:40:24 +0530

    Initial changes for first Android App - MintedRich
    Initial changes for first Android App - MintedRich

---
+++ MintedRich/app/src/main/java/minted/christina/denisa/com/mintedrich/MainActivity.java
@@ -0,0 +1,13 @@
+package minted.christina.denisa.com.mintedrich;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
